89cb87a7ae97ea9b88baa124d3ed3c1376e73a4f
ThunderRedStar <thunderredstar@gmail.com>
2022-02-13 22:02:06 -0500

    futura futuris oh so futuristic
    Signed-off-by: ThunderRedStar <thunderredstar@gmail.com>

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 
 module.exports = async member => {
 	// find the welcome channel in the cache, if not found find the general channel, if not found find the system channel,
@@ -10,5 +11,15 @@ module.exports = async member => {
 		}
 	}
 
+	// create a new canvas, this is where the welcome image will be drawn
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	// load the fonts in the fonts folder
+
+	// main font for stuypulse website
+	Canvas.registerFont('./fonts/avenir.otf', { family: 'Avenir' })
+	// main font in stuypulse logo
+	Canvas.registerFont('./fonts/futura.ttf', { family: 'Futura' })
 
 }
